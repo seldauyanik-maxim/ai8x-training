4190d27343727b626bc45122b6aaf948ca9b1212
Selda Uyanik <selda.uyanik@maximintegrated.com>
2022-11-10 02:39:21 -0600

    Add dummy test file for pytest workflow

---
+++ test_plain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+###################################################################################################
+#
+# Copyright (C) 2021 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+Test routine combining all available regression test cases for pytest run
+"""
+
+def test():
+    """Test routine for nas implementation"""
+    assert(True, 'FAIL!!')
+    print('PASS')
+
+
+if __name__ == "__main__":
+    test()
